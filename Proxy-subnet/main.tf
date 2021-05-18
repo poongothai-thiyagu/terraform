ab62c1d8e42cc680364f5b586f37717b06693f1e
poongothai-thiyagu <54898265+poongothai-thiyagu@users.noreply.github.com>
2021-05-18 20:39:11 +0530

    Update main.tf

--- Proxy-subnet/main.tf
+++ Proxy-subnet/main.tf
@@ -1,1 +1,13 @@
-resource 
+resource "google_compute_network" "proxy" {
+  name = "proxy"
+  auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "subnet" {
+  provider = google-beta
+  name          = "proxy-subnet"
+  network       = google_compute_network.proxy.id
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "asia-south1"
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
+}
