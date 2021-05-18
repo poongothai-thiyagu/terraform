0a476c4f071615ec568663008e788351cec760f7
poongothai-thiyagu <54898265+poongothai-thiyagu@users.noreply.github.com>
2021-05-18 18:15:56 +0530

    Create main.tf
    It creates a VPC, a subnet, a router and cloud nat gateway

---
+++ Cloud NAT/main.tf
@@ -0,0 +1,34 @@
+resource "google_compute_network" "test-network" {
+  name = "test-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "test-subnetwork"
+  network       = google_compute_network.test-network.id
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "asia-south1"
+}
+
+resource "google_compute_router" "router" {
+  name    = "router"
+  region  = google_compute_subnetwork.subnet.region
+  network = google_compute_network.test-network.id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
