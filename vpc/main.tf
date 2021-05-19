4203a93a6035cca89316030703d0948d6e6465f7
poongothai-thiyagu <poongothai12thiyagu@gmail.com>
2021-05-19 11:54:16 +0530

    change-network

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_network" "new" {
-  name                    = "new"
+resource "google_compute_network" "vpc" {
+  name                    = "vpc"
   auto_create_subnetworks = false
 }
 
@@ -8,7 +8,7 @@ resource "google_compute_subnetwork" "public" {
   name          = "public"
   ip_cidr_range = "10.0.0.0/24"
   region        = "asia-south1"
-  network       = google_compute_network.new.id
+  network       = google_compute_network.vpc.id
 }
 
 # Private Subnet
@@ -16,7 +16,7 @@ resource "google_compute_subnetwork" "private" {
   name          = "private"
   ip_cidr_range = "10.0.1.0/24"
   region        = "asia-south1"
-  network       = google_compute_network.new.id
+  network       = google_compute_network.vpc.id
   private_ip_google_access= true
 }
 
