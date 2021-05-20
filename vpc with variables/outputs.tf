709e550498803009f77f6b7783c1fd9bad432f80
poongothai-thiyagu <54898265+poongothai-thiyagu@users.noreply.github.com>
2021-05-20 19:58:47 +0530

    Delete vpc with variables directory

--- vpc with variables/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "network" {
-  value       = google_compute_network.network
-  description = "The VPC resource being created"
-}
-
-output "network_name" {
-  value       = google_compute_network.network.name
-  description = "The name of the VPC being created"
-}
