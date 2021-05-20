356fa8566d5ee12a1c16bc306cfb186034816357
poongothai-thiyagu <poongothai12thiyagu@gmail.com>
2021-05-20 20:49:17 +0530

    add outputs.tf file

---
+++ VPC&subnet-variables/outputs.tf
@@ -0,0 +1,9 @@
+output "network" {
+  value       = google_compute_network.network
+  description = "The VPC resource being created"
+}
+
+output "network_name" {
+  value       = google_compute_network.network.name
+  description = "The name of the VPC being created"
+}
